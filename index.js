1e94d4f64dbbd89d5fdff4ed65b28e9de9c6f4a1
Ali Aftab <110173898+ali-aftab-khokhar@users.noreply.github.com>
2022-08-13 23:40:32 +0500

    Single Paranthesis: Fixed

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const PI = 3.1415
-const e = 2.7182
+const constants = {
+    'PI': 3.1415,
+    'e' : 2.7182
+}
 const variable = {}
-const variableExist = false;
+var elements = []
+var operators = []
+var mOperator = []
+var mElments = []
 
 function values(el){
     let x = document.getElementById("answer");
@@ -17,45 +22,129 @@ function values(el){
 function calculate(){
     var x = document.getElementById("answer");
     var array = x.value.split(" ")
-    var temp = array.shift()
-    // variableVal = isVariable(temp, array)
-    // if (temp !== variableVal){
-    //     variableExist = true;
-    //     variable.variableVal = 0
-    // }
-    temp = isNull(temp, array)
-    temp = parseFloat(isSpecialCase(temp, array))
-    var calculation = switchCases(0, "+", parseInt(temp))
-    while (array.length != 0){
-        var sign = array.shift();
-        var num = array.shift();
-        num = isNull(num, array)
-        num = isSpecialCase(num, array)
-        calculation = switchCases(calculation, sign, parseInt(num))
-    }
-    if (variableExist){
-        variable.variableVal = calculation;
-    }
-    var old = x.value;
-    description = old + " = " + calculation;
-    x.value = description
-    displayHistory(description)
-}
+    console.log(array)
 
-// function isVariable(temp, array){
-//     if (typeof(temp) === String && temp !== 'cos' && temp !== 'tan' && temp !== 'sin' && temp != 'PI' && temp !== 'E'){
-//         return temp
-//     }
-// }
+    //Elements Array
+    array.forEach(elem => {
+        if (parseInt(elem) / 1 === parseInt(elem)){
+            elements.push(parseInt(elem))
+        }
+        else if (elem === ''){
+            console.log("Undefined Entry")
+        }
+        // else if (Object.keys(constants).includes(elem)){
+        //     elements.push(parseFloat(constants[elem]))
+        // }
+        else{
+            operators.push(elem)
+        }
+    });
 
-function isSpecialCase(num, array){
-    if (num === 'sqrt' || num === 'sin' || num === 'cos' || num === 'tan' || num === "PI" || num ==="E" ){
-        temp = array.shift();
-        num = specialSwitchCase(num, temp)
-        return num;
+    // console.log(elements)
+    // console.log(operators)
+
+    while (operators.includes("(")){
+        //Solving Paranthesis
+        console.log(elements)
+        solvingParanthesis(operators, elements)
+    }
+
+    //Solving Single Valued Operator
+    solvingSingleValuedOperator(operators, elements)
+
+    //By DMAS Law
+    calculateByDMAS("^", operators, elements)
+    calculateByDMAS("/", operators, elements)
+    calculateByDMAS("*", operators, elements)
+    calculateByDMAS("+", operators, elements)
+    calculateByDMAS("-", operators, elements)   
+
+    // console.log(elements, 'elements')
+    // console.log(operators, 'operators')
+
+    //Displaying On Console
+    var tempHist = x.value
+    var calculatedAnswer = elements.shift()
+    if (calculatedAnswer % 1 !== 0){
+        x.value = calculatedAnswer.toFixed(4)
     }
     else{
-        return num;
+        x.value = calculatedAnswer
+    }
+    elements = []
+    displayHistory(tempHist + " = " + calculatedAnswer.toFixed(4))
+
+}
+
+function solvingParanthesis(operators, elements){
+    var opening = 0
+    var closing = 0
+    console.log(elements)
+    console.log(operators)
+    var i
+    for (i = 0; i < operators.length; i += 1){
+        if (operators[i] === '('){
+            opening = i
+        }
+        if (operators[i] === ')'){
+            closing = i
+        }
+    }
+    miniArray(opening, closing)
+}
+
+function miniArray(opening, closing){
+    mOperator = operators.splice(opening, closing - 1)
+    mElments = elements.splice(opening, closing - 2)
+
+    //Solving Single Valued Operator
+    solvingSingleValuedOperator(mOperator, mElments)
+
+    //By DMAS Law
+    calculateByDMAS("^", mOperator, mElments,closing)
+    calculateByDMAS("/", mOperator, mElments,closing)
+    calculateByDMAS("*", mOperator, mElments,closing)
+    calculateByDMAS("+", mOperator, mElments,closing)
+    calculateByDMAS("-", mOperator, mElments,closing)
+
+    elements.splice(opening - 1, 0, mElments.shift())
+    console.log(elements)
+    console.log(operators)
+}
+
+function solvingSingleValuedOperator(operators, elements){
+    var i
+    for (i = 0; i < operators.length; i++){
+        if (operators[i] === 'sqrt' || operators[i] === 'sin' || operators[i] === 'cos' || operators[i] === 'tan'){
+            var tempVal = 0;
+            tempVal = specialSwitchCase(operators[i], elements[i])
+            elements[i] = tempVal
+            operators.splice(i, 1)
+        }
+    }
+}
+
+function calculateByDMAS(oper, operators, elements, closing = 2){
+    for (i = 0; i < operators.length; i++){
+        if (operators[i] === oper){
+            if (oper === '/'){
+                tempVal = elements[i] / elements[i+1]
+            }
+            else if (oper === '*'){
+                tempVal = elements[i] * elements[i+1]
+            }
+            else if (oper === '+'){
+                tempVal = elements[i] + elements[i+1]
+            }
+            else if (oper === '-'){
+                tempVal = elements[i] - elements[i+1]
+            }
+            else if (oper === '^'){
+                tempVal = elements[i] ** elements[i+1]
+            }
+            elements.splice(i, closing, tempVal)
+            operators.splice(i, 1)
+        }
     }
 }
 
@@ -75,26 +164,19 @@ function isNull(temp, array){
 }
 
 function specialSwitchCase(oper, num){
-    console.log(num)
-    num = parseInt(num)
+    console.log(oper, num)
     switch (oper){
         case 'sqrt':
-            return Math.sqrt(num).toFixed(4)
+            return parseFloat(Math.sqrt(num).toFixed(4))
 
         case 'sin':
-            return Math.sin(num).toFixed(4)
+            return parseFloat(Math.sin(num).toFixed(4))
         
         case 'cos':
-            return Math.cos(num).toFixed(4)
+            return parseFloat(Math.cos(num).toFixed(4))
 
         case 'tan':
-            return Math.tan(num).toFixed(4)
-
-        case 'PI':
-            return (PI*num).toFixed(4)
-
-        case 'E':
-            return (e*num).toFixed(4)
+            return parseFloat(Math.tan(num).toFixed(4))
 
         default:
             return num
@@ -127,42 +209,3 @@ function clearConsole(el){
     var x = document.getElementById("answer");
     x.value = ''
 }
-
-// function values(el) {
-//     var val = parseInt(el.value)
-//     temp = (temp * 10) + val
-//     document.getElementById("answer").innerHTML = temp
-// }
-
-// function settingValue(){
-//     num.push(temp)
-//     temp = 0
-//     console.log(num)
-// }
-
-// function sign(el) {
-//     settingValue()
-//     if (num.length - 2 === oper.length){
-//         oper.push(el.value)
-//     }
-// }
-
-// function calculate(){
-//     console.log(num)
-//     console.log(oper)
-//     calculatedAnswer = calculatedAnswer + num.shift()
-//     while(num.length != 0){
-//         calculatedAnswer = switchCases(calculatedAnswer, num.shift(), oper.shift())
-//     }
-//     console.log(calculatedAnswer)
-// }
-
-// function trigno(el){
-//     settingValue()
-//     if (el.value === "sin")
-//         document.getElementById("answer").innerHTML = Math.sin(num.shift())
-//     else if (el.value === "cos")
-//         document.getElementById("answer").innerHTML = Math.cos(num.shift())
-//     else if (el.value === "tan")
-//         document.getElementById("answer").innerHTML = Math.tan(num.shift())
-// }
